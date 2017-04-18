46c0f65d6e92669b8948601ca5e8d61b0e830247
jerrek <jerrek@mail.ru>
2017-04-18 13:23:12 +0300

    First

---
+++ Micron Memory driver for stm32/Define.h
@@ -0,0 +1,35 @@
+
+
+//Memory
+
+#define MEM_PAGE_SIZE 2048
+#define MEM_BLOCK_QUANTITY 262144
+#define MEM_DATA_ARRAY 40
+#define QUANTITY_SECONDS_IN_YEAR 31556926
+#define MEMBusy 0
+#define MEMReady 1
+#define MEMTIME 10
+#define MEM_OK 1
+#define MEM_BAD 0
+
+#define MEM_WRITE_START 0x80
+#define MEM_WRITE_END 0x10
+
+#define MEM_TWO_PLANE_WRITE_START 0x80
+#define MEM_TWO_PLANE_WRITE_END 0x11
+
+#define MEM_READ_START 0x00
+#define MEM_READ_END 0x30
+
+#define MEM_TWO_PLANE_READ_START1 0x00
+#define MEM_TWO_PLANE_READ_START2 0x00
+#define MEM_TWO_PLANE_READ_END 0x30
+
+#define MEM_ERASE_START 0x60
+#define MEM_ERASE_END 0xD0
+
+#define MODBUS 1
+#define ARHIV 2
+
+#define GIGABYTE 1073741824
+
---
+++ Micron Memory driver for stm32/Memory.c
@@ -0,0 +1,870 @@
+#include "stm32f10x_gpio.h"
+
+
+#include "define.h"
+
+extern uint8_t MemoryError;
+
+
+void ReinitMemsPinsForOuts (void);
+void ReinitMemsPinsForIns (void);
+volatile uint8_t MEMReadData (void);
+void MEMWriteData (volatile uint8_t byte);
+void MEMWriteAdress (volatile uint8_t byte);
+void MEMWriteCommand (volatile uint8_t byte);
+uint8_t MEM_Ready_Busy (void);
+void MEMRead (uint8_t status);
+void MEMWrite (uint8_t status);
+void MEMAddress (uint8_t status);
+void MEMCommand(uint8_t status);
+void MEMChipSelect (uint8_t status);
+void MEMWriteProtect (uint8_t status);
+uint8_t ReadStatus(void);
+void ReadManufacturID( uint8_t *data);
+void MEMReset(void);
+uint8_t MEMInit(void);
+void MEMCycleWriteData (volatile uint8_t *byte, uint8_t *adr);
+void MEMCycleReadData (uint8_t reset, uint8_t *adr,  uint8_t *data);
+void MEMEraseData (uint8_t reset, uint8_t *adr);
+
+void ReinitMemsPinsForOuts (void)//set your pin number
+{
+	GPIO_InitTypeDef  GPIO_InitStructure;
+
+	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15 ; // IO7 | IO6 |IO5 | IO4
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;//уточнить
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin   =   GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 ; // IO3 | IO2 | IO1 | IO0
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;//уточнить
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+	GPIO_ResetBits(GPIOC, GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 );
+	GPIO_ResetBits(GPIOB, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+
+}
+
+void ReinitMemsPinsForIns (void)//set your pin number
+{
+	GPIO_InitTypeDef  GPIO_InitStructure;
+
+	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15 ; // IO7 | IO6 |IO5 | IO4
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IN_FLOATING;//уточнить
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin   =   GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 ; // IO3 | IO2 | IO1 | IO0
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IN_FLOATING;//уточнить
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+	//GPIO_ResetBits(GPIOC, GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_12 | GPIO_Pin_13);
+	//GPIO_ResetBits(GPIOA, GPIO_Pin_11 |  GPIO_Pin_12 |  GPIO_Pin_15);
+
+}
+
+volatile uint8_t MEMReadData (void)// read 1 byte
+{
+	volatile uint8_t data=0;
+	uint8_t data_correct=0;
+	uint8_t confirm=0;
+	uint8_t c=0;
+	uint8_t i=0;
+
+
+	ReinitMemsPinsForIns ();
+    //while(MEM_Ready_Busy())
+	delay(MEMTIME);
+
+	if(MEM_Ready_Busy()==MEMReady)
+	{
+		  MEMChipSelect(ENABLE);
+
+		  MEMRead(ENABLE);
+			for(i=0;i<10;i++)
+			{
+			  if((GPIOC->IDR & GPIO_Pin_9))
+			  {
+				   data=(data|0x01);
+			  }
+			  else
+			  {
+				   data=(data|0x00);
+			  }
+
+			  if((GPIOC->IDR & GPIO_Pin_8))
+			  {
+				   data=(data|0x02);
+			  }
+			  else
+			  {
+				   data=(data|0x00);
+			  }
+
+			  if((GPIOC->IDR & GPIO_Pin_7))
+			  {
+				   data=(data|0x04);
+			  }
+			  else
+			  {
+				   data=(data|0x00);
+			  }
+
+			  if((GPIOC->IDR & GPIO_Pin_6))
+			  {
+				   data=(data|0x08);
+			  }
+			  else
+			  {
+				   data=(data|0x00);
+			  }
+
+			  if((GPIOB->IDR & GPIO_Pin_15))
+			  {
+				   data=(data|0x10);
+			  }
+			  else
+			  {
+				   data=(data|0x00);
+			  }
+
+			  if((GPIOB->IDR & GPIO_Pin_14))
+			  {
+				   data=(data|0x20);
+			  }
+			  else
+			  {
+				   data=(data|0x00);
+			  }
+
+			  if((GPIOB->IDR & GPIO_Pin_13))
+			  {
+				   data=(data|0x40);
+			  }
+			  else
+			  {
+				   data=(data|0x00);
+			  }
+
+			  if((GPIOB->IDR & GPIO_Pin_12))
+			  {
+				   data=(data|0x80);
+			  }
+			  else
+			  {
+				   data=(data|0x00);
+			  }
+			  if(c>0)
+			  {
+				if(data_correct==data)
+				{
+					confirm++;
+					if(i>=9)
+					{
+						if(confirm>=8)
+						{
+							break;
+						}
+						else {
+							i=0;
+						}
+					}
+				}
+			  }
+			  else
+			  {
+				  data_correct=data;
+				  c=1;
+			  }
+			}
+		  MEMRead(DISABLE);
+
+		  MEMChipSelect(DISABLE);
+
+	}
+
+	//MemLedStatusChange();
+
+	  return data_correct;
+
+}
+
+void MEMWriteData (volatile uint8_t byte)// write 1 data byte
+{
+	ReinitMemsPinsForOuts();
+
+	volatile uint8_t shift=0;
+	volatile uint16_t i=0;
+
+	delay(MEMTIME);
+
+	 for(i=0;i<10000;i++)
+	  if(MEM_Ready_Busy())
+	   {
+		 MEMChipSelect(ENABLE);
+
+		 MEMRead (DISABLE);
+
+		 MEMCommand(DISABLE);
+
+		 MEMAddress(DISABLE);
+
+		 MEMWrite(ENABLE);
+
+              if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     				GPIO_SetBits(GPIOC, GPIO_Pin_9 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOC, GPIO_Pin_9 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOC, GPIO_Pin_8 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOC, GPIO_Pin_8 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOC, GPIO_Pin_7 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOC, GPIO_Pin_7 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOC, GPIO_Pin_6 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOC, GPIO_Pin_6 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOB, GPIO_Pin_15 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOB, GPIO_Pin_15 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOB, GPIO_Pin_14 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOB, GPIO_Pin_14 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOB, GPIO_Pin_13 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOB, GPIO_Pin_13 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOB, GPIO_Pin_12 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOB, GPIO_Pin_12 );
+     		  }
+     		  shift=0;
+     		  break;
+	   }
+
+     		 delay(MEMTIME);
+		 //MEMChipSelect(ENABLE);
+
+		// MEMCommand(DISABLE);
+
+		// MEMAddress(DISABLE);
+
+		 MEMWrite(DISABLE);
+
+
+			//MemLedStatusChange();
+
+}
+
+void MEMWriteAdress (volatile uint8_t byte)// write 1 addres byte
+{
+
+	ReinitMemsPinsForOuts();
+
+	volatile uint8_t shift=0;
+	volatile uint16_t i=0;
+
+	delay(MEMTIME);
+
+	 for(i=0;i<10000;i++)
+	  if(MEM_Ready_Busy())
+	   {
+
+		 MEMChipSelect(ENABLE);
+
+		 MEMRead (DISABLE);
+
+		 MEMCommand(DISABLE);
+
+		 MEMAddress(ENABLE);
+
+		 MEMWrite(ENABLE);
+
+
+
+              if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     				GPIO_SetBits(GPIOC, GPIO_Pin_9 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOC, GPIO_Pin_9 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOC, GPIO_Pin_8 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOC, GPIO_Pin_8 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOC, GPIO_Pin_7 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOC, GPIO_Pin_7 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOC, GPIO_Pin_6 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOC, GPIO_Pin_6 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOB, GPIO_Pin_15 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOB, GPIO_Pin_15 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOB, GPIO_Pin_14 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOB, GPIO_Pin_14 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOB, GPIO_Pin_13 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOB, GPIO_Pin_13 );
+     		  }
+     		  shift++;
+
+     		  if(((byte>>shift) & 0x01)==0x01)
+     		  {
+     			    GPIO_SetBits(GPIOB, GPIO_Pin_12 );
+     		  }
+     		  else
+     		  {
+     			  GPIO_ResetBits(GPIOB, GPIO_Pin_12 );
+     		  }
+     		  shift=0;
+     		 break;
+	   }
+
+     		 delay(MEMTIME);
+		// MEMChipSelect(ENABLE);
+
+		// MEMCommand(DISABLE);
+
+          MEMWrite(DISABLE);
+
+		 MEMAddress(DISABLE);
+
+			//MemLedStatusChange();
+
+}
+
+void MEMWriteCommand (volatile uint8_t byte)// write 1 command byte
+{
+
+	ReinitMemsPinsForOuts();
+
+	volatile uint8_t shift=0;
+	volatile uint16_t i=0;
+
+	delay(MEMTIME);
+
+	 for(i=0;i<10000;i++)
+	  if(MEM_Ready_Busy())
+	   {
+		 MEMCommand(ENABLE);
+
+		 MEMWrite(ENABLE);
+
+		 MEMChipSelect(ENABLE);
+
+		 MEMRead (DISABLE);
+
+		 MEMAddress(DISABLE);
+
+		  if(((byte>>shift) & 0x01)==0x01)
+			  {
+					GPIO_SetBits(GPIOC, GPIO_Pin_9 );
+			  }
+			  else
+			  {
+				  GPIO_ResetBits(GPIOC, GPIO_Pin_9 );
+			  }
+			  shift++;
+
+			  if(((byte>>shift) & 0x01)==0x01)
+			  {
+					GPIO_SetBits(GPIOC, GPIO_Pin_8 );
+			  }
+			  else
+			  {
+				  GPIO_ResetBits(GPIOC, GPIO_Pin_8 );
+			  }
+			  shift++;
+
+			  if(((byte>>shift) & 0x01)==0x01)
+			  {
+					GPIO_SetBits(GPIOC, GPIO_Pin_7 );
+			  }
+			  else
+			  {
+				  GPIO_ResetBits(GPIOC, GPIO_Pin_7 );
+			  }
+			  shift++;
+
+			  if(((byte>>shift) & 0x01)==0x01)
+			  {
+					GPIO_SetBits(GPIOC, GPIO_Pin_6 );
+			  }
+			  else
+			  {
+				  GPIO_ResetBits(GPIOC, GPIO_Pin_6 );
+			  }
+			  shift++;
+
+			  if(((byte>>shift) & 0x01)==0x01)
+			  {
+					GPIO_SetBits(GPIOB, GPIO_Pin_15 );
+			  }
+			  else
+			  {
+				  GPIO_ResetBits(GPIOB, GPIO_Pin_15 );
+			  }
+			  shift++;
+
+			  if(((byte>>shift) & 0x01)==0x01)
+			  {
+					GPIO_SetBits(GPIOB, GPIO_Pin_14 );
+			  }
+			  else
+			  {
+				  GPIO_ResetBits(GPIOB, GPIO_Pin_14 );
+			  }
+			  shift++;
+
+			  if(((byte>>shift) & 0x01)==0x01)
+			  {
+					GPIO_SetBits(GPIOB, GPIO_Pin_13 );
+			  }
+			  else
+			  {
+				  GPIO_ResetBits(GPIOB, GPIO_Pin_13 );
+			  }
+			  shift++;
+
+			  if(((byte>>shift) & 0x01)==0x01)
+			  {
+					GPIO_SetBits(GPIOB, GPIO_Pin_12 );
+			  }
+			  else
+			  {
+				  GPIO_ResetBits(GPIOB, GPIO_Pin_12 );
+			  }
+			  shift=0;
+			  break;
+
+	   }
+
+		  delay(MEMTIME);
+
+	     MEMWrite(DISABLE);
+
+		 //MEMChipSelect(ENABLE);
+
+		 //MEMAddress(DISABLE);
+
+		 MEMCommand(DISABLE);
+
+			//MemLedStatusChange();
+}
+
+uint8_t MEM_Ready_Busy (void)//hardware confirm Ready/Busy
+{
+	uint8_t status=0;
+
+	if((GPIOA->IDR & GPIO_Pin_7))
+	{
+		status=MEMReady;
+	}
+	else
+	{
+		status=MEMBusy;
+	}
+
+	return status;
+
+}
+
+void MEMRead (uint8_t status)// Read mode
+{
+	if(status==ENABLE)
+	{
+		GPIO_ResetBits(GPIOC, GPIO_Pin_4);
+	}
+	else if(status==DISABLE)
+	{
+		GPIO_SetBits(GPIOC, GPIO_Pin_4);
+	}
+
+    delay(MEMTIME);
+}
+
+void MEMWrite (uint8_t status)// Write mode
+{
+	if(status==ENABLE)
+	{
+		GPIO_ResetBits(GPIOB, GPIO_Pin_10);
+	}
+	else if(status==DISABLE)
+	{
+		GPIO_SetBits(GPIOB, GPIO_Pin_10);
+	}
+
+    delay(MEMTIME);
+}
+
+void MEMAddress (uint8_t status)// Addres Write mode
+{
+	if(status==ENABLE)
+	{
+		GPIO_SetBits(GPIOB, GPIO_Pin_2);
+	}
+	else if(status==DISABLE)
+	{
+		GPIO_ResetBits(GPIOB, GPIO_Pin_2);
+	}
+
+    delay(MEMTIME);
+}
+
+void MEMCommand(uint8_t status)// Command Write mode
+{
+	if(status==ENABLE)
+	{
+		GPIO_SetBits(GPIOB, GPIO_Pin_1);
+	}
+	else if(status==DISABLE)
+	{
+		GPIO_ResetBits(GPIOB, GPIO_Pin_1);
+	}
+
+    delay(MEMTIME);
+}
+
+void MEMChipSelect (uint8_t status)// Chip Select
+{
+	if(status==ENABLE)
+	{
+		GPIO_ResetBits(GPIOC, GPIO_Pin_5);
+		GPIO_ResetBits(GPIOB, GPIO_Pin_0);
+	}
+	else if(status==DISABLE)
+	{
+		GPIO_SetBits(GPIOC, GPIO_Pin_5);
+		GPIO_SetBits(GPIOB, GPIO_Pin_0);
+	}
+
+    delay(MEMTIME);
+}
+
+void MEMWriteProtect (uint8_t status)// Protect from write data
+{
+	if(status==ENABLE)
+	{
+		GPIO_ResetBits(GPIOB, GPIO_Pin_11);
+	}
+	else if(status==DISABLE)
+	{
+		GPIO_SetBits(GPIOB, GPIO_Pin_11);
+	}
+
+    delay(MEMTIME);
+}
+
+uint8_t ReadStatus(void)// read status byte
+{
+	uint8_t data=0;
+
+	 MEMWriteCommand(0x70);
+	 data=MEMReadData();
+
+	 return data;
+}
+
+void ReadManufacturID( uint8_t *data)// Read ID bytes
+{
+	uint8_t i=0;
+
+	 MEMWriteCommand(0x90);
+	 MEMWriteAdress(0x00);
+	 for(i=0;i<5;i++)
+	 data[i]=MEMReadData();
+}
+
+void MEMReset(void)// Reset chip
+{
+	 MEMWriteCommand(0xFF);
+	 delay(10000);
+}
+
+uint8_t MEMInit(void)// Memory chip init
+{
+
+	 volatile uint8_t status=0;
+
+     if(MEM_Ready_Busy())
+     {
+	  MEMReset();
+     }
+
+	 status=ReadStatus();
+
+	 if((status==0xE0))
+	   return MEM_OK;
+	 else
+	   return MEM_BAD;
+
+}
+
+void MEMEraseData (uint8_t reset, uint8_t *adr)// Erase 1 block FLASH memory
+{
+
+	MEMChipSelect(ENABLE);
+	MEMWriteProtect(DISABLE);
+
+		uint8_t i=0;
+		uint8_t status=0;
+		static uint8_t count=0;
+
+		if(count>=255)
+		{
+		MemLedStatusChange();
+		count=0;
+		}
+		else
+		count++;
+
+		if(reset==0xFF)
+		{
+			for(i=0;i<3;i++)
+				adr[i]=0;
+		}
+		else
+		{
+				status = ReadStatus();
+			   if(((status>>6) & 0x01)==1)
+			   {
+
+
+					MEMWriteCommand(MEM_ERASE_START);
+
+					for(i=0;i<3;i++)
+						MEMWriteAdress(adr[i]);
+
+					MEMWriteCommand(MEM_ERASE_END);
+
+					status = ReadStatus();
+					if((status & 0x01)==0)
+					{
+						status=0xFF;
+					}
+
+			   }
+
+		}
+
+		MEMWriteProtect(ENABLE);
+		MEMChipSelect(DISABLE);
+
+}
+
+void MEMCycleWriteData (volatile uint8_t *byte, uint8_t *adr)// write 1 page FLASH memory
+{
+
+	MEMChipSelect(ENABLE);
+	MEMWriteProtect(DISABLE);
+
+	uint16_t i=0;
+	uint16_t i1=0;
+	uint16_t i2=0;
+	uint16_t i3=0;
+
+	uint8_t status=0;
+
+	for(i=0;i<10000;i++)
+  {
+
+			status = ReadStatus();
+		   if(((status>>6) & 0x01)==1)
+		   {
+			 for(i1=0;i1<10;i1++)
+			  if(MEM_Ready_Busy())
+			   {
+
+					    MEMWriteCommand(MEM_WRITE_START);
+
+						for(i2=0;i2<5;i2++)
+							MEMWriteAdress(adr[i2]);
+
+						for(i3=0;i3<MEM_PAGE_SIZE;i3++)
+						MEMWriteData(byte[i3]);
+
+						MEMWriteCommand(MEM_WRITE_END);
+
+						status = ReadStatus();
+						if((status & 0x01)==0)
+						{
+							status=0xFF;
+
+							 break;
+						}
+						else{
+							status=0x00;
+						}
+
+			   }
+					 break;
+           }
+  }
+
+	MEMWriteProtect(ENABLE);
+	MEMChipSelect(DISABLE);
+
+}
+
+
+void MEMCycleReadData (uint8_t reset, uint8_t *adr,  uint8_t *data)// read 1 page FLASH memory
+{
+	MEMChipSelect(ENABLE);
+	MEMWriteProtect(DISABLE);
+
+	uint32_t i=0;
+	uint32_t i1=0;
+	uint32_t i2=0;
+
+	uint8_t status=0;
+
+
+	static uint8_t count=0;
+
+	if(count>=255)
+	{
+	MemLedStatusChange();
+	count=0;
+	}
+	else count++;
+
+
+	//MemLedStatusChange();
+	if(reset==0xFF)
+	{
+		for(i=0;i<5;i++)
+			adr[i]=0;
+	}
+	else
+	{
+
+			for(i=0;i<10000;i++)
+			  {
+
+						status = ReadStatus();
+					   if(((status>>6) & 0x01)==1)
+					   {
+							MEMWriteCommand(MEM_READ_START);
+
+									for(i1=0;i1<5;i1++)
+										MEMWriteAdress(adr[i1]);
+
+									MEMWriteCommand(MEM_READ_END);
+
+									for(i2=0;i2<MEM_PAGE_SIZE;i2++)
+									data[i2]=MEMReadData();
+
+									status = ReadStatus();
+									if((status & 0x01)==0)
+									{
+										status=0xFF;
+									}
+
+
+						 break;
+
+					   }
+			  }
+    }
+
+	MEMWriteProtect(ENABLE);
+	MEMChipSelect(DISABLE);
+
+}
